4da4ceb99d5b60968d561a82e7ba5f1f8be17784
Steven Mackey <stevester94@gmail.com>
2021-02-07 15:52:29 -0800

    Simple example of FFT and IFFT

---
+++ fft_and_back.m
@@ -0,0 +1,45 @@
+Fs = 10000;
+duration_secs = 2;
+t = [0:1/Fs:duration_secs];
+
+% Build the AM signal
+amp_carrier = 20;
+freq_carrier = 4500;
+target_modulation_index = 1;
+freq_m = 5 * sin(2*pi*t) + 10;
+amp_m = sin(2*pi*t) + 1;
+m = (amp_m) .* sin(2*pi*t .* freq_m);
+m = m + sin(2*pi*10*t);
+modulation_index = max(m) / amp_carrier;
+m = m ./ (modulation_index/target_modulation_index);
+x = (amp_carrier + m) .* sin(2 * pi * freq_carrier * t);
+
+tiledlayout(4,1);
+
+nexttile;
+plot(m);
+
+
+nexttile;
+plot(x);
+
+L = numel(t);
+Y = fft(x);
+P2 = abs(Y);
+P2 = P2/L;
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+% The frequency axis. We have L/2 bins, and Fs/2 Hz spread across them. So
+% that is (Fs/2)/(L/2) Hz per Bin = Fs/L Hz Per Bin
+f = Fs/L*(0:(L/2));
+
+nexttile;
+plot(f,P1) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+
+x_inv = ifft(Y);
+nexttile;
+plot(x_inv);
